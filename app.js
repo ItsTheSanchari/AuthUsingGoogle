5dd1daad26182e709b8016f492437d42a27d6b3f
ItsTheSanchari <sanchari678@gmail.com>
2021-06-19 18:36:32 +0530

    using clientid from env

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ app.set('view engine','ejs')
 app.use(express.json())
 app.use(cookieParser())
 app.get('/',(req,res,next)=> {
+    console.log('client id',clientId)
     res.render('index',{
         clientId:clientId,
 
     })
 })
-
+app.post('/login',(req,res,next) => {
+    console.log('client id',req.body)
+})
 app.listen(port,() => {
     console.log('server running on ',port)
 })
