2a97fb8dfb20599418f05e1151eb363db2ad1081
ItsTheSanchari <sanchari678@gmail.com>
2021-06-20 19:33:10 +0530

    adding middleware

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
-const clientId = process.env.clientId
-const { OAuth2Client } = require('google-auth-library');
+const clientId = process.env.clientId;
+const { OAuth2Client } = require("google-auth-library");
 const client = new OAuth2Client(clientId);
 //middleware
-app.set('view engine','ejs')
-app.use(express.json())
-app.use(cookieParser())
-app.get('/',(req,res,next)=> {
-    console.log('client id',clientId)
-    res.render('index',{
-        clientId:clientId,
-
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(cookieParser());
+app.get("/", (req, res, next) => {
+  console.log("client id", clientId);
+  res.render("index", {
+    clientId: clientId,
+  });
+});
+app.post("/login", (req, res, next) => {
+  console.log("client id", req.body);
+  async function verify() {
+    const ticket = await client.verifyIdToken({
+      idToken: req.body.token,
+      audience: clientId,
+    });
+    const payload = ticket.getPayload();
+    const userid = payload["sub"];
+    console.log("payload", payload);
+  }
+  verify()
+    .then(() => {
+      console.log("verified");
     })
-})
-app.post('/login',(req,res,next) => {
-    console.log('client id',req.body)
-    async function verify() {
-        const ticket = await client.verifyIdToken({
-            idToken: req.body.token,
-            audience: clientId,
-        });
-        const payload = ticket.getPayload();
-        const userid = payload['sub'];
-        console.log('payload',payload)
-      }
-      verify().then(()=> {
-          console.log('verified')
-      }).catch(console.error);
-})
-app.listen(port,() => {
-    console.log('server running on ',port)
-})
+    .catch(console.error);
+});
+app.listen(port, () => {
+  console.log("server running on ", port);
+});
--- middleware/verify.js
+++ middleware/verify.js
@@ -3,5 +3,8 @@ const clientId = process.env.clientId
 const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(clientId);
 exports.verify = (req,res,next) => {
-
+    const data = verifyToken()
+}
+verifyToken = () => {
+    
 }
